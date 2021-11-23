f309ec9909ee0183f604c0bfcecd15df467426b5
jarquinigor <jarquinigorupc@gmail.com>
2021-11-23 12:33:33 -0500

    Update de logeo
    update de logeo

--- src/main/java/pe/edu/upc/spring/controller/LoginController.java
+++ src/main/java/pe/edu/upc/spring/controller/LoginController.java
@@ -60,7 +60,7 @@ public class LoginController {
 			//AÑADIREMOS ROLES
 			List<Role>roles = new ArrayList<Role>();
 			Role role = new Role();
-			role.setAuthority("ROLE_USER");
+			role.setAuthority("ROLE_ADMIN");
 			roles.add(role);
 			objUser.setRoles(roles);
 			
